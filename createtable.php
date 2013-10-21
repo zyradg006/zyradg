569cb674f5fd7ba7b773703a08e3d2e34019ba94
Zyra Rae T. de Grano <zyradg006@yahoo.com>
2013-10-21 22:19:52 +0800

    exer3

---
+++ createtable.php
@@ -0,0 +1,23 @@
+<?php
+// DB connection info
+$host = "tcp:lcqy05lo5v.database.windows.net,1433";
+$user = "zyradg";
+$pwd = "Vaselinespf24";
+$db = "tasklist";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+?>
+
